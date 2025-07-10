26cc2ea8c60932fb60a2edf60037b262d90bb25c
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-10 13:02:32 +0500

    commit

---
+++ Chapter3/string.py
@@ -0,0 +1,16 @@
+name= "abcdefghijklmnopqrstuvwxyz"
+print(name)
+
+#Slicing in String
+name2= name[0:20]  
+print(name2)
+
+#printing single character from a string
+
+name3=name[5]
+print(name3)
+
+#Slicing selective Characters from a string
+
+name4=name[0:26:4]
+print(name4)
