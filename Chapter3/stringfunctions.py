42f286c6edba56fb5c1db5e4b46a16d383f711da
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-10 13:21:54 +0500

    commit

---
+++ Chapter3/stringfunctions.py
@@ -0,0 +1,14 @@
+name = "Muhammad"
+
+print(len(name))   #length function
+
+name2 = name.capitalize()     #This function will capitalize the 1st letter of the string
+print(name2)
+
+
+name3 = name.replace('u','o')   #this is function for replacing character
+print(name3)
+
+
+name4 = name.count('m')   # this function will count the number of m in the string 'name'
+print(name4)
