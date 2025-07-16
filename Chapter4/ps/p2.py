52c88618a7b4bbb08c4dad9e93a8689335f9d3b4
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-16 22:13:00 +0500

    commit

--- Chapter4/ps/p2.py
+++ Chapter4/ps/p2.py
@@ -1,12 +1,12 @@
 marks = []
 
-m1 = input("Enter the marks of student1: ")
+m1 = int(input("Enter the marks of student1: "))
 marks.append(m1)
-m2 = input("Enter the marks of student2: ")
+m2 = int(input("Enter the marks of student2: "))
 marks.append(m2)
-m3 = input("Enter the marks of student3: ")
+m3 = int(input("Enter the marks of student3: "))
 marks.append(m3)
-m4 = input("Enter the marks of student4: ")
+m4 = int(input("Enter the marks of student4: "))
 marks.append(m4)
 
 marks.sort()
---
+++ Chapter4/ps/p4.py
@@ -0,0 +1,5 @@
+l1 = [3, 56, 192, 91]
+
+# we will use sum function to add these values
+
+print(sum(l1))
