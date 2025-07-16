ca799f220803afa293d5fb4135f3a80c615e5a23
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-16 21:54:19 +0500

    commit

---
+++ Chapter4/tuple.py
@@ -0,0 +1,3 @@
+a = (1, 4, 5, "Ahmad")
+print(a)
+print(type(a))
---
+++ Chapter4/tupleMethods.py
@@ -0,0 +1,7 @@
+t = (2, 5, 66, 7, 88, 66)
+
+no = t.count(66)
+print(no)    #conut no of 66
+
+y = t.index(5)
+print(y)     # give the index of 5
