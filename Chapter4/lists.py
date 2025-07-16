7383a6059a0f761d824900375b6d638ba0f813d1
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-16 21:18:52 +0500

    commit

--- Chapter4/lists.py
+++ Chapter4/lists.py
@@ -1,3 +1,9 @@
-students = ["Ahmad", "Ali", "Umer", 4 , False]
+students = ["Ahmad", "Ali", "Umer", 4 , False]  
 
 print(students)
+
+#lists are mutable
+
+students [0] = "Usman"
+
+print(students[0])
