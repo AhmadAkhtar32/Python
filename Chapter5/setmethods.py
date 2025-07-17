418ac0dd3142e37fd72bd7936c56ec3b163681f4
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-17 21:14:32 +0500

    commit

---
+++ Chapter5/setmethods.py
@@ -0,0 +1,14 @@
+a = { 1, 2, 4, 6, 6, 7, 8, 8,8 , "Ahmad"}  
+
+print(type(a))
+
+#add method to add value in sets
+a.add(123)
+print(a)
+
+#operations of sets
+
+
+print(len(a))
+a.remove(4)
+print(a)
