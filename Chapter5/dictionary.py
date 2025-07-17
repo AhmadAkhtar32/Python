8e0b70a26dfc39a98517ba74991fabd3d027fd67
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-07-17 20:59:52 +0500

    commit

--- Chapter5/dictionary.py
+++ Chapter5/dictionary.py
@@ -1,6 +1,17 @@
 marks = {
     "Ahamd": 96,
     "Ali" : 90,
-    "Haider": 80
+    "Haider": 80,
+    0: "CMD"
 }
 print(marks, type(marks))
+print(marks["Ahamd"])
+print(marks.keys())
+
+#updating the dict
+marks.update({"Ahmad":99})
+print(marks)
+
+#
+print(marks.get("Ahmad1"))  #will print none as key is not found
+print(marks["Ahamd"])
