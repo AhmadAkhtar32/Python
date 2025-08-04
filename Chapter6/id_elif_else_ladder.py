ed93853cc220cc08f9f7d435894b4c2b5561d431
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-04 21:59:08 +0500

    commit

---
+++ Chapter6/id_elif_else_ladder.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your Age: "))
+
+if (a>=18) : 
+    print("You are eligible!")
+
+elif (a<0):
+    print("plese enter a valid age!")
+
+elif (a==0):
+    print("Age can't be Zero")
+
+else:
+    print("You are not eligible!")
