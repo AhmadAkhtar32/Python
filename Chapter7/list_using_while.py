2853697f5c3b08a52c44017ed1d4e3d57545a0b5
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-06 15:37:11 +0500

    commit

---
+++ Chapter7/list_using_while.py
@@ -0,0 +1,7 @@
+l = ["Ahmad", 32 , "Ahsan", True, 100, "AT the Mall", 45, False, "Perfect" ]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i +=1
--- Chapter7/while_loops.py
+++ Chapter7/while_loops.py
@@ -1,5 +1,9 @@
+#In While loop conditions are checked first , if it evaluates to true, the body of loop is executed otherwise not
+#if loop is entered the process is continued untill the condition becomes false
+
+
 i = 1
 
-while (i<6):
+while (i<51):
     print(i)
     i +=1
