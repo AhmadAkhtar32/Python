2138161aa89bd8be74498c07724bafe6365c11c0
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-06 18:23:36 +0500

    commit

---
+++ Chapter7/for_else.py
@@ -0,0 +1,7 @@
+l = [10, 15 ]
+
+for i in l:
+    print(i)
+
+else:
+    print("done")
---
+++ Chapter7/for_loops.py
@@ -0,0 +1,13 @@
+for i in range(0, 100, 4):
+    print(i)
+
+# printing list using for loop
+
+l = [ 2, 34 ,43, 56, 98, 87,203]
+for i in l: 
+    print(i)
+
+# For String
+s = "Ahsan"
+for i in s :
+    print(i)
