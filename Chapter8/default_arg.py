2af794f6ef30abf59493b449f5a9ad35dbd9b395
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-07 23:13:14 +0500

    commit

--- Chapter8/default_arg.py
+++ Chapter8/default_arg.py
@@ -3,4 +3,5 @@ def hey(name, ending="Thank You !"):
     print(f"Greetings, {name}")
     print(ending)
 
-hey("Ahmad")
+hey("Ahmad")  #will call default value for ending
+hey("Ahsan ", "Thanks")  # will take new value for ending
