1018d38ef4bf5224b4b589edbcb5822a4068be57
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-07 22:31:55 +0500

    commit

---
+++ Chapter8/functions.py
@@ -0,0 +1,12 @@
+#Defining a Funtion
+def avg():
+    a = int(input("Enter The Number: "))
+    b = int(input("Enter The Number: "))
+    c = int(input("Enter The Number: "))
+
+    avg = (a + b+c)/3
+    print(avg)
+
+# Calling a function
+
+avg()
