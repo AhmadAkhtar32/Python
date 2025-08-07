78bc8c721979b4ac7ba4f45e1aa67cd109745012
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-07 23:49:38 +0500

    commit

---
+++ Chapter8/ps/p7.py
@@ -0,0 +1,10 @@
+# Function For Removing Word from List
+
+def rem(l, word):
+    for item in l:
+        l.remove(word)
+        return l
+
+l = ["Ahmad", "Ahsan", "Ah", "Ahish"]
+
+print(rem(l, "Ah"))
