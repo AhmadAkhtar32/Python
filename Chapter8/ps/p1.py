a95d6b49ac97efecf3592a0ed76e0406a8cf8881
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-07 23:28:35 +0500

    commit

---
+++ Chapter8/ps/p1.py
@@ -0,0 +1,13 @@
+a = int(input("Enter The Number: "))
+b = int(input("Enter The Number: "))
+c = int(input("Enter The Number: "))
+
+def grt():
+    if(a>b and a>c):
+        print(f"The Greatest Number is: {a}")
+    elif(b>a and b>c):
+        print(f"The Greatest Number is: {b}")
+    elif(c>b and c>a):
+        print(f"The Greatest Number is: {c}")
+
+grt()
---
+++ Chapter8/ps/p2.py
@@ -0,0 +1,8 @@
+# Converting temperatiue From F to C
+
+def f_to_c(f):
+     return 5*(f-32)/9
+    
+    
+f=int(input("Enter The Temperature In F: "))
+print(f_to_c(f))
