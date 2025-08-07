82e5caee24845d9d6a0e4cb76bf154f157b491cc
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-07 22:51:05 +0500

    commit

---
+++ Chapter8/default_arg.py
@@ -0,0 +1,6 @@
+#Functions with default arguments
+def hey(name, ending="Thank You !"):
+    print(f"Greetings, {name}")
+    print(ending)
+
+hey("Ahmad")
---
+++ Chapter8/fun_arguments.py
@@ -0,0 +1,7 @@
+# Functions With Arguments
+def hey(name, ending):
+    print("Greetings! " + name)
+    print(ending)
+
+hey("Ahmad", "Thank You !")
+hey("Ahsan", "Thank You !")
