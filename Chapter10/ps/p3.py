8fea469dc8d501d7b7851ab93881b24a9cc7b4e3
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-11 00:27:34 +0500

    commit

---
+++ Chapter10/ps/p3.py
@@ -0,0 +1,10 @@
+class Test:
+    a = 5  # Class attribute
+
+obj = Test()
+print("Before change:", Test.a)  # Output: 5
+
+# Changing via object
+obj.a = 0
+print("After object change:", obj.a)  # Output: 0 (only for this object)
+print("Class attribute still:", Test.a)  # Output: 5 (unchanged)
