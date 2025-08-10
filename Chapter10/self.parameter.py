062f08c6e2a2d5c3caa39744120164110d4bcd82
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-08-10 23:57:12 +0500

    commit

---
+++ Chapter10/self.parameter.py
@@ -0,0 +1,13 @@
+class Car:
+    name = "Civic"
+    status = "Available"
+    Company = "Honda"
+    Price = "9000000"
+
+    def getinfo(self):
+        print(f"The Company is: {self.Company}. The Price is: {self.Price}")
+
+vehicle = Car()
+print(Car.name, Car.Company, Car.Price, Car.status)
+
+Car.getinfo(vehicle)
