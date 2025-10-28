87ee99a4edf5147bcf03e2e89d4f683aa7f7e60f
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-10-28 23:40:50 +0500

    commit

--- AC/Python_Course/variables/operators.py
+++ AC/Python_Course/variables/operators.py
@@ -6,3 +6,13 @@ Relational/Comparison operators(==, !=, >, <)
 Assignment Operators (=, +=, -=)
 Logical Operators (not, and , or)
 """
+
+# In Type-Conversion : Python automatically converts From 1 type to Other variable
+a = 20
+b = 89.09
+sum = a+b
+
+# In Type-Casting : Manually converting type of variables
+a = int("20")
+b = 90
+sum = a+b
