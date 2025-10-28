bd5675196c25ad92eddbd78864d0630729a5f3db
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-10-28 23:33:30 +0500

    commit

---
+++ AC/Python_Course/variables/operators.py
@@ -0,0 +1,8 @@
+"""
+An Operator is a symbol that performs a certain operations between operands.
+
+Arithmetic operators(+,-,/,*,%,**)
+Relational/Comparison operators(==, !=, >, <)
+Assignment Operators (=, +=, -=)
+Logical Operators (not, and , or)
+"""
