09b26d6118e76d8196233adbaedb3174502def32
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-10-28 23:53:03 +0500

    commit

---
+++ AC/Python_Course/variables/input.py
@@ -0,0 +1,14 @@
+# The input() function allows user input.
+name = input("Enter Your Name: ")
+age = input("Enter Age: ")
+print("Your Name Is:", name)
+print("Type of name is:", type(name))
+
+print("Your Age Is:", age)
+print("Type of age is:", type(age))
+
+# Result of Input Statement in Python is always a string
+
+val = int(input())
+print("Your Value Is:", val)
+print("Type of val is:", type(val))
