05857776e80990097f2c0c0c40b69a12ed91724f
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-10-29 00:00:43 +0500

    commit

---
+++ AC/Python_Course/Chp1/datatypes.py
@@ -0,0 +1,26 @@
+# 5 Common Data types
+#1:_ Integer
+#2:_ String
+#3:_ Float
+#4:_ Boolean
+#5:_ None
+
+age = 23
+old = False
+a = None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+c = 5 
+d = 10 
+sum = c+d
+print(sum)
+
+# Single Line Comments
+
+"""
+Multi-Line
+Comment
+"""
---
+++ AC/Python_Course/Chp1/practice.py
@@ -0,0 +1,11 @@
+# Write a program to input 2 numbers and find their sum
+a = int(input("Enter the value of A:"))
+b = int(input("Enter the value of B:"))
+sum = a+b
+print("The Sum is: ", sum)
+
+# Write a program to input sides of a square & print its area
+h = input("Enter the Width: ")
+w = input("Enter the length: ")
+area = h*w
+print("The Area Is: ", area)
