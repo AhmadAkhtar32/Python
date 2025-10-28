49bfab8f258f12f1600b45d534367891c0ffbd6a
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-10-29 00:12:42 +0500

    commit

--- AC/Python_Course/Chp1/practice.py
+++ AC/Python_Course/Chp1/practice.py
@@ -1,11 +1,26 @@
-# Write a program to input 2 numbers and find their sum
+# # Write a program to input 2 numbers and find their sum
 a = int(input("Enter the value of A:"))
 b = int(input("Enter the value of B:"))
 sum = a+b
 print("The Sum is: ", sum)
 
-# Write a program to input sides of a square & print its area
-h = input("Enter the Width: ")
-w = input("Enter the length: ")
+# # Write a program to input sides of a square & print its area
+h = int(input("Enter the Width: "))
+w = int(input("Enter the length: "))
 area = h*w
 print("The Area Is: ", area)
+
+
+#Write a program to input 2 floating point numbers and find their average
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+average = (num1 + num2) / 2
+print("The average is:", average)
+
+
+# Write a program to input 2 int numbers, z and x, print True if z is greater than or equal to x, if not print False
+
+z = int(input("Enter value of z: "))
+x = int(input("Enter value of x: "))
+result = z >= x
+print(result)
