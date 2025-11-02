05537cc97d8e933e2ca8cc33c64b7425ab4d748c
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-02 12:17:30 +0500

    commit

--- AC/Python_Course/Chp2/strings.py
+++ AC/Python_Course/Chp2/strings.py
@@ -18,3 +18,16 @@ print(str1)
 #Example
 #a = "Hello"
 #print(a)
+
+
+#Multiline Strings
+#You can assign a multiline string to a variable by using three quotes:
+
+#Example
+#You can use three double quotes:
+
+#a = """Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor incididunt
+#ut labore et dolore magna aliqua."""
+#print(a)
