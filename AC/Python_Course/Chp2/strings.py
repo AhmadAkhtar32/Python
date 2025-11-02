7ebc735e5aeb4a5920e9d26b70a4abf624155888
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-02 12:16:24 +0500

    commit

--- AC/Python_Course/Chp2/strings.py
+++ AC/Python_Course/Chp2/strings.py
@@ -11,3 +11,10 @@ print("Hello"  +  "Developers")
 str1 = "This is the First String"
 
 print(str1)
+
+#Assign String to a Variable
+#Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+
+#Example
+#a = "Hello"
+#print(a)
