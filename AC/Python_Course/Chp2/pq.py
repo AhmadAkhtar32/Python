9e090c411e940094fb008b362d9abae385aa2ad3
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-11 22:56:27 +0500

    commit

---
+++ AC/Python_Course/Chp2/pq.py
@@ -0,0 +1,9 @@
+# WAP to input user's first-name and print its length
+
+f_name = input("Enter your first name: ")
+print(len(f_name))
+
+# WAP to find the occurences of '$' in a string
+str1 = "My income is 3000£. But i count it in $. so, according to $ value my pay is ~4000$"
+count_dollar = str1.count('$')
+print("Nummber of $ in str1 is: ", count_dollar)
