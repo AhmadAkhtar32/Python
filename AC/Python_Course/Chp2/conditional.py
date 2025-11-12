89a21c167fbdf1668382e81fe42ed4f50b00529a
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-12 17:57:04 +0500

    commit

--- AC/Python_Course/Chp2/conditional.py
+++ AC/Python_Course/Chp2/conditional.py
@@ -1,2 +1,9 @@
 #The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
 #The elif keyword allows you to check multiple expressions for True and execute a block of code as soon as one of the conditions evaluates to True.
+
+a = 33
+b = 33
+if a>b :
+    print("A is greater than B")
+elif a ==b:
+    print("A is equal to B")
