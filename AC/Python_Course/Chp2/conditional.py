e58ec3f8a23e2feb0f212a43405e759099a7b26a
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-12 17:54:01 +0500

    commit

---
+++ AC/Python_Course/Chp2/conditional.py
@@ -0,0 +1,2 @@
+#The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+#The elif keyword allows you to check multiple expressions for True and execute a block of code as soon as one of the conditions evaluates to True.
