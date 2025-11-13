771cd213c2d117ffe5cd13fc433fe8ade54e5567
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-14 00:42:45 +0500

    commit

--- AC/Python_Course/Chp3/tuple.py
+++ AC/Python_Course/Chp3/tuple.py
@@ -1,2 +1,5 @@
 # A buit-in data type that lets us create immutable sequences of values
-
+# In tuple we use () brackets instead of []
+tup = (2, 1, 3, 1, 5, 1)
+print(tup.index(1)) 
+print(tup.count(1)) 
