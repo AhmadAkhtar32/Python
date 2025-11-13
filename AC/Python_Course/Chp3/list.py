e5bcfa5e4ae8f2817dfed91e6c2c4325d9ce5f68
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-13 18:03:09 +0500

    commit

---
+++ AC/Python_Course/Chp3/list.py
@@ -0,0 +1,10 @@
+# Lists in Python
+# A built-in data type that stores set of values
+# marks = [87, 64, 33, 95, 76]
+# It can store elements of different types (integer, float, string, etc.)
+
+marks = ["Ahmad Rao", 12.34 , 23, 66.25 , True , 34]
+print(marks)
+print(marks[0])      # printing a specific value in List
+
+#Strings are immutable in Python
