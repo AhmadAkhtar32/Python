4d80ad44dbabca8545ce583aa0c8cb331bd14d23
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-14 23:22:28 +0500

    commit

--- AC/Python_Course/Chp4/dictionary.py
+++ AC/Python_Course/Chp4/dictionary.py
@@ -16,3 +16,21 @@ print(info)
 # Tuple: Ordered, indexed, immutable collection allowing duplicates.
 # Dictionary: Unordered key–value pairs, mutable, keys must be unique.
 # Set: Unordered, mutable collection of unique elements only.
+
+
+# Nested Dictionary
+student = {
+    "Name": "Ahmad",
+    "Score": {
+        "DSA": 90,
+        "OOp": 85,
+        "PF": 91
+    }
+}
+
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items())
+student.update({"City": "Islamabad"})   # Can add new value and also can update previous_one
+print("After Update:", student)
---
+++ AC/Python_Course/Chp4/sets.py
@@ -0,0 +1,3 @@
+# Set in Python
+# Set is the collection of the unordered items.
+# Each element in the set must be unique & immutable.
