4015174c132c9344fa3db3becaf25f3e7df4e43f
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-14 22:26:53 +0500

    commit

--- AC/Python_Course/Chp4/dictionary.py
+++ AC/Python_Course/Chp4/dictionary.py
@@ -3,3 +3,16 @@
 # “key” : value
 # They are unordered, mutable(changeable) & don’t allow duplicate keys
 
+info = {
+    "Name": ["Ahmad", "Ali", "Asad"],
+    "Age": 23,
+    "Education": "Graduate",
+    "Cities": ("Lahore", "Isalamabad", "Sahiwal")
+}
+print(info)
+
+# Tuple vs List vs Set vs Dictionary
+# List: Ordered, indexed, mutable collection allowing duplicates.
+# Tuple: Ordered, indexed, immutable collection allowing duplicates.
+# Dictionary: Unordered key–value pairs, mutable, keys must be unique.
+# Set: Unordered, mutable collection of unique elements only.
