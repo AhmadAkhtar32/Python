c8fba926f288fc746684edd036da60213e0de34a
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-11-16 21:55:13 +0500

    commit

--- AC/Python_Course/Chp5/loops.py
+++ AC/Python_Course/Chp5/loops.py
@@ -2,3 +2,11 @@
 # Python has two primitive loop commands:
 # while loops
 # for loops
+
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+count = 1
+while count <=5 :
+    print("Hello")
+    count += 1
