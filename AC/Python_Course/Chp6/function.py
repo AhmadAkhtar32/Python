1f00c3705945bf1a9d3e107a506f11f43bca638f
Ahmad Akhtar <ahmadrao3226@gmail.com>
2025-12-26 19:54:44 +0500

    Commit

--- AC/Python_Course/Chp6/function.py
+++ AC/Python_Course/Chp6/function.py
@@ -1,2 +1,3 @@
 # Functions In PYTHON
-#Block of statements that perform a specific task.
+# A function is a reusable block of code that performs a specific task.
+# Instead of writing the same code repeatedly, you define it once and call it whenever needed.
