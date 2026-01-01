29be8532fb1a847306507a544c955c8e4be6e406
Ahmad Akhtar <ahmadrao3226@gmail.com>
2026-01-01 21:12:15 +0500

    commit

--- AC/Python_Course/Chp6/function.py
+++ AC/Python_Course/Chp6/function.py
@@ -1,3 +1,4 @@
 # Functions In PYTHON
 # A function is a reusable block of code that performs a specific task.
 # Instead of writing the same code repeatedly, you define it once and call it whenever needed.
+# Functions are main part 
